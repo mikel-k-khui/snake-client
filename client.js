84b863c2ac33e93e8aba6e68fa353a6b10d8921a
Mike Chui <mikel.k.khui@gmail.com>
2019-07-31 13:04:13 -0600

    Updated constants for IP, PORT, and WASD

--- client.js
+++ client.js
@@ -1,13 +1,24 @@
 const net = require('net');
-const { IP, PORT } = require('./constants');
+const consts = require('./constants');
+
+// const move = function(direction, interval, loops) {
+//   let count = 0;
+//   console.log(consts[direction], loops, interval);
+//   while (count < loops) {
+//     setTimeout(function() {
+//       conn.write(consts[direction]);
+//     },interval*count);
+//     ++count;
+//   }
+// };
 
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({
-    host: IP,
-    port: PORT
+    host: consts["IP"],
+    port: consts["PORT"]
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
@@ -24,16 +35,6 @@ const connect = function() {
     conn.write("Name: MKC");
     // conn.write("Move: down");
     // conn.write("Move: down");
-    // // const move = function(direction, interval, loops) {
-    //   let count = 0;
-    //   console.log(moves[direction], loops, interval);
-    //   while (count < loops) {s
-    //     setTimeout(function() {
-    //       conn.write(moves[direction]);
-    //     },interval);
-    //     ++count;
-    //   }
-    // };
     // move("up", 50, 5);
   });
   //end of initial connection
